eb39fc841c79c1775077453360d6e306992f4a72
Sabuhi Teymurov <teymurov.sabuhi@gmail.com>
2025-02-21 00:11:02 +0400

    [ios][location] fix iOS `LocationAlways` permission inaccurate status
    Changed location manager initialization from async to sync to ensure the permission result waits for user interaction. This fixes the issue where requestBackgroundPermissionsAsync() would return "denied" immediately when the permission dialog appears, instead of waiting for user's choice. The promise was resolving too early with a "denied" status before the user could make their choice.

--- packages/expo-location/ios/Requesters/EXBaseLocationRequester.m
+++ packages/expo-location/ios/Requesters/EXBaseLocationRequester.m
@@ -34,7 +34,7 @@
 # pragma mark - UMPermissionsRequester
 
 - (NSDictionary *)getPermissions {
-
+  
   CLAuthorizationStatus systemStatus;
   if (![EXBaseLocationRequester isConfiguredForAlwaysAuthorization] && ![EXBaseLocationRequester isConfiguredForWhenInUseAuthorization]) {
     EXFatal(EXErrorWithMessage(@"This app is missing usage descriptions, so location services will fail. Add one of the `NSLocation*UsageDescription` keys to your bundle's Info.plist. See https://bit.ly/3iLqy6S (https://docs.expo.dev/distribution/app-stores/#system-permissions-dialogs-on-ios) for more information."));
@@ -56,14 +56,12 @@
     _resolve = resolve;
     _reject = reject;
 
-    EX_WEAKIFY(self)
-    [EXUtilities performSynchronouslyOnMainThread:^{
-      EX_ENSURE_STRONGIFY(self)
+    dispatch_async(dispatch_get_main_queue(), ^{
       self.locationManagerWasCalled = false;
       self.locationManager = [[CLLocationManager alloc] init];
       self.locationManager.delegate = self;
-    }];
-
+      [self requestLocationPermissions];
+    });
     // 1. Why do we call CLLocationManager methods by those dynamically created selectors?
     //
     //    Most probably application code submitted to Apple Store is statically analyzed
@@ -91,8 +89,6 @@
     //        user responded to the dialog selecting `whenInUse` or is still deciding
     //    To support this use case we will have to change the way location authorization is requested
     //    from promise-based to listener-based.
-
-    [self requestLocationPermissions];
   }
 }
 
@@ -119,26 +115,6 @@
     // http://stackoverflow.com/questions/30106341/swift-locationmanager-didchangeauthorizationstatus-always-called/30107511#30107511
     _locationManagerWasCalled = true;
     return;
-  }
-
-  if (_resolve) {
-    _resolve([self getPermissions]);
-    _resolve = nil;
-    _reject = nil;
-  }
-}
-
-- (void)locationManagerDidChangeAuthorization:(CLLocationManager *)manager
-{
-  CLAuthorizationStatus status = [manager authorizationStatus];
-  if (status == kCLAuthorizationStatusNotDetermined || !_locationManagerWasCalled) {
-    // CLLocationManager calls this delegate method once on start with kCLAuthorizationNotDetermined even before the user responds
-    // to the "Don't Allow" / "Allow" dialog box. This isn't the event we care about so we skip it. See:
-    // http://stackoverflow.com/questions/30106341/swift-locationmanager-didchangeauthorizationstatus-always-called/30107511#30107511
-    _locationManagerWasCalled = true;
-    if (status != kCLAuthorizationStatusAuthorizedWhenInUse) {
-      return;
-    }
   }
 
   if (_resolve) {
